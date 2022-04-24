eee9fb5d082a331413961fbd6ef4e1a924fa6ba9
Shloka <shlokareddy@gmail.com>
2022-04-24 14:58:09 -0700

    built product information query and route

--- server/index.js
+++ server/index.js
@@ -4,12 +4,19 @@ const models = require('./models/models.js');
 const app = express();
 const port = 3000;
 
-// Get all products
+// List products
 app.get('/products', (req, res) => {
   models.readAllProducts(req.query.page || 1, req.query.count || 5)
-    .then((products) => res.send(products.rows))
+    .then((products) => res.status(200).send(products.rows))
     .catch((err) => res.sendStatus(500));
 });
+
+// Product information
+app.get('/products/:product_id', (req, res) => {
+  models.readProductInfo(req.params.product_id)
+    .then((product) => res.status(200).send(product.rows[0]))
+    .catch((err) => res.sendStatus(500));
+})
 
 app.listen(port, () => {
   console.log('listening on port:', port);
--- server/models/models.js
+++ server/models/models.js
@@ -1,9 +1,32 @@
 let db = require('../db/db.js');
 
 const readAllProducts = (page, count) => {
-  const text = 'select * from products offset $1 limit $2';
+  const text = 'SELECT * FROM products OFFSET $1 LIMIT $2';
   const values = [(count * (page - 1)), count];
   return db.query(text, values);
 }
 
-module.exports = { readAllProducts };
+const readProductInfo = (product_id) => {
+  const query = `
+    SELECT
+      p.id,
+      p.name,
+      p.slogan,
+      p.description,
+      p.category,
+      p.default_price,
+      (SELECT json_agg(
+        json_build_object(
+          'feature', f.feature,
+          'value', f.value)
+        )
+        FROM features f
+        WHERE f.product_id = p.id
+      ) features
+    FROM products p
+    WHERE p.id = ${product_id}
+  `;
+  return db.query(query);
+}
+
+module.exports = { readAllProducts, readProductInfo };
