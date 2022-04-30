27e9f55070a6c74da564cfafd96b3d3f951a31bf
Shloka <shlokareddy@gmail.com>
2022-04-30 10:48:40 -0700

    changed user and password

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'test_user',
+  user: 'postgres',
   host: '18.144.62.20',
   database: 'products',
-  port: 5432
+  port: 5432,
+  password: 'password'
 });
 
 pool.connect((err, res) => {
