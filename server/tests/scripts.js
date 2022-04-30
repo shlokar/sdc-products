d522215f64ca9b6aadefacecb6193829d8c9c72f
Shloka <shlokareddy@gmail.com>
2022-04-29 17:01:29 -0700

    k6 tests

--- server/tests/scripts.js
+++ server/tests/scripts.js
@@ -2,6 +2,8 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
+  InsecureSkipTLSVerify: true,
+  noConnectionReuse: false,
   stages: [
     { duration: '10s', target: 100 },
     { duration: '1m', target: 100 },
@@ -10,10 +12,11 @@ export let options = {
     { duration: '10s', target: 100 },
     { duration: '3m', target: 100 },
     { duration: '10s', target: 0 },
-  ],
+  ]
 }
 
 export default function () {
-  http.get('https://localhost:3000/products');
+  http.get(`http://localhost:3000/products`);
+  // http.get(`http://localhost:3000/products/${Math.floor(Math.random() * 1000000)}/related`);
   sleep(1);
 }
